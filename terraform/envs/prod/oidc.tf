23704437387e75cacbaefbe201805d483823acb1
lotachio <lotachio@hardcorebiometric.com>
2026-09-06 13:24:11 +0100

    Add additional ECR actions to GitHub Actions IAM policy

--- terraform/envs/prod/oidc.tf
+++ terraform/envs/prod/oidc.tf
@@ -22,7 +22,7 @@ resource "aws_iam_policy" "github_deploy_policy" {
         Action = [
           "ecr:BatchCheckLayerAvailability", "ecr:PutImage", "ecr:InitiateLayerUpload",
           "ecr:UploadLayerPart", "ecr:CompleteLayerUpload", "ecr:BatchGetImage",
-          "ecr:DescribeRepositories", "ecr:DescribeImages", "ecr:ListTagsForResource" , "ecr:GetRepositoryPolicy", 
+          "ecr:DescribeRepositories", "ecr:DescribeImages", "ecr:ListTagsForResource", "ecr:GetRepositoryPolicy",
           "ecr:GetLifecyclePolicy"
         ]
         Resource = "arn:aws:ecr:${var.aws_region}:716542960555:repository/${var.project_name}*"
